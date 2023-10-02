e70e2f7dd5a02ea973dacfa19ac49965ee95d7c0
Antoine Rondelet <rondelet.antoine@gmail.com>
2023-10-02 19:38:48 +0100

    Added basic redirect logic

---
+++ redirect.js
@@ -0,0 +1,9 @@
+// Detect device 
+var isAndroid = /Android/i.test(navigator.userAgent);
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+if(isAndroid) {
+  window.location.href = "/playstore"; 
+} else if(isIOS) {
+  window.location.href = "/appstore";
+}
