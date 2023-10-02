15736ec06f7c437160002df0e218a6296d3716d0
Antoine Rondelet <rondelet.antoine@gmail.com>
2023-10-02 20:36:53 +0100

    Added desktop redirect

--- redirect.js
+++ redirect.js
@@ -2,8 +2,10 @@
 var isAndroid = /Android/i.test(navigator.userAgent);
 var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-if(isAndroid) {
-  window.location.href = "/playstore"; 
-} else if(isIOS) {
-  window.location.href = "/appstore";
+if (isAndroid) {
+    window.location.href = "/playstore";
+} else if (isIOS) {
+    window.location.href = "/appstore";
+} else {
+    window.location.href = "/desktop";
 }
